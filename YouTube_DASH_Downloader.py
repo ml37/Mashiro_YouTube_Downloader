1e2bd76c0e90cb3f7507793da3ad700fac624166
ml37 <richiemu0416@gmail.com>
2022-06-24 14:41:02 +0900

    Update YouTube_DASH_Downloader.py

--- YouTube_DASH_Downloader.py
+++ YouTube_DASH_Downloader.py
@@ -1,16 +1,23 @@
 from pytube import YouTube
 import tkinter
+import sys
 #url=input("Enter the url: ")
 def on_progress(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
+    '''total_size = stream.filesize
     bytes_downloaded = (total_size - bytes_remaining)
     bytes_remaining_MB = round(bytes_remaining*0.000001,1)
     done_size = round((total_size - bytes_remaining)*0.000001,1)
     percentage_of_completion = round(bytes_downloaded / total_size * 100, 1)
+    print(f'{percentage_of_completion}%, {done_size}MB/{bytes_remaining_MB}MB')'''
+    global filesize
+    current = ((filesize - bytes_remaining)/filesize)
+    percent = ('{0:.1f}').format(current*100)
+    progress = int(50*current)
+    status = '█' * progress + '-' * (50 - progress)
+    sys.stdout.write(' ↳ |{bar}| {percent}%\r'.format(bar=status, percent=percent))
+    sys.stdout.flush()
 
-    print(f'{percentage_of_completion}%, {done_size}MB/{bytes_remaining_MB}MB')
-
-url='https://www.youtube.com/watch?v=6I5nor_880M' #For test
+url='https://www.youtube.com/watch?v=eyv9jO6kTkY' #For test
 yt=YouTube(url)
 print('--------------------')
 print(f'Video Title : {yt.title}')
