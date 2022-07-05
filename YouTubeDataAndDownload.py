4a95a5e1c4bd1af21030db792cf2c3b2275fc41f
ml37 <richiemu0416@gmail.com>
2022-07-05 14:25:38 +0900

    Update YouTubeDataAndDownload.py
    lighter code and work

--- YouTubeDataAndDownload.py
+++ YouTubeDataAndDownload.py
@@ -1,4 +1,3 @@
-from matplotlib.image import thumbnail
 import pytube
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -14,33 +13,32 @@ class WindowClass(QMainWindow, form_class) :
         super().__init__()
         self.setupUi(self)
         self.progressBar.setValue(0)
-        self.btn_load.clicked.connect(self.walalaru)
+        self.btn_load.clicked.connect(self.comboboxload)
         self.btn_download.clicked.connect(self.download)
         self.lbl_len.setText("0")
-        self.comboBox.currentIndexChanged.connect(self.on_comboBox_changed)
     def download(self):
         print('download')
-        print(streamhaschanged)
-        streamhaschanged.download()
+        for video in ch.videos:
+            if video.title == self.comboBox.currentText():
+                stream = video.streams.filter(progressive=True).order_by('resolution').desc().first()
+                stream.download()
+                print('download complete')
+                self.progressBar.setValue(100)
 
-    def on_comboBox_changed(self):
+    '''def on_comboBox_changed(self):
         print('comboBox changed')
         print(self.comboBox.currentText())
         print(connecttitleandnum[self.comboBox.currentText()])
         stream = connecttitleandnum[self.comboBox.currentText()]
         print(stream.filter(progressive=True).order_by('resolution').desc().first())
         global streamhaschanged
-        streamhaschanged = stream.filter(progressive=True).order_by('resolution').desc().first()
-    def walalaru(self):
-        global connect
+        streamhaschanged = stream.filter(progressive=True).order_by('resolution').desc().first()'''
+    '''def walalaru(self):
         global connecttitleandnum
         global thumbnailurl
-        connect = {'title': ''}
         connecttitleandnum = {'title': ''}
         self.comboBox.clear()
         ch = Channel('https://www.youtube.com/channel/UCAjWiWgGWg1ingZUoRLVgKw')
-        print('=' * 50)
-        print(len(ch))
         self.lbl_len.setText(str(len(ch)))
         length = len(ch)
         count = 0
@@ -48,22 +46,22 @@ class WindowClass(QMainWindow, form_class) :
         percent = ('{0:.1f}').format(current*100)
         for video in ch.videos:
             count = count + 1
-            connect[count] = video.streams
             connecttitleandnum[video.title] = video.streams
             thumbnailurl = video.thumbnail_url
             current = count/length
             percent = ('{0:.0f}').format(current*100)
-            print(video.title)
-            print(video)
             self.comboBox.addItem(video.title)
-            self.progressBar.setValue(int(percent))
-            print(f'current: {current}')
-            print(f'percent: {percent}')
-            print(f'count: {count}')
-            print(f'length: {length}')
-        print(connect)
-        print('=' * 50)
-        print(connecttitleandnum)
+            self.progressBar.setValue(int(percent))'''
+    def comboboxload(self):
+        global ch
+        self.comboBox.clear()
+        ch = Channel('https://www.youtube.com/channel/UCAjWiWgGWg1ingZUoRLVgKw')
+        self.lbl_len.setText(str(len(ch)))
+        length = len(ch)
+        count = 0
+        for video in ch.videos:
+            count = count + 1
+            self.comboBox.addItem(video.title)
             
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
